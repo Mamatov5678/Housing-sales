18d18daf0a3caa35366b14dbc3987f156cb499fd
Mamatov5678 <mamatovorolboy5678@gmail.com>
2022-08-24 17:43:20 +0500

    add component

---
+++ app/Http/Livewire/AddListingComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class AddListingComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.add-listing-component');
+    }
+}
---
+++ app/Http/Livewire/LocationsComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class LocationsComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.locations-component');
+    }
+}
